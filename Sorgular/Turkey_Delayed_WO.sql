dce2dbfa7323525bc2fc4216ec9684cc078c5652
aytunc matrac <aytunc.matrac@hotmail.com.tr>
2015-06-27 18:47:45 +0300

    Workorder detail update
    Workorder detail raporu düzenlemesi

--- Sorgular/Turkey_Delayed_WO.sql
+++ Sorgular/Turkey_Delayed_WO.sql
@@ -28,14 +28,14 @@ where 1=1
 select workorder.wonum, workorder.description,  
 (select top 1 jobplan.priority from jobplan, workorder where jobplan.jpnum=workorder.jpnum and ((jobplan.orgid=workorder.orgid and jobplan.siteid=workorder.siteid) or (jobplan.orgid = workorder.orgid and jobplan.siteid is null) or (jobplan.orgid is null and jobplan.siteid is null)) ) as jpriority,  
 (select top 1 site.description from site, workorder where site.siteid=workorder.siteid) as siteid ,  
-workorder.targstartdate,workorder.targcompdate,workorder.actfinish,workorder.actfinish-workorder.targcompdate as delay   
+workorder.targstartdate,workorder.targcompdate,workorder.actfinish, Convert(int, convert(datetime,workorder.actfinish)) - Convert(int, Convert(datetime,workorder.targcompdate)) as delay   
 from workorder  
 where 1=1 and ((workorder.siteid like '%YARIMCA%')) and workorder.orgid='YARORG' and (workorder.parent = 'NULL' or workorder.parent is null) and workorder.jpnum is not null and workorder.targstartdate is not null and workorder.targcompdate is not null  and workorder.targcompdate <= { ts '2015-01-01 23:59:59.998' } and workorder.targstartdate >= { ts '2009-01-01 00:00:00.000' }
 
 
-0542 308 20 52 ercan ekıcı 
+select workorder.wonum, workorder.description,  (select top 1 jobplan.priority from jobplan, workorder where jobplan.jpnum=workorder.jpnum and ((jobplan.orgid=workorder.orgid and jobplan.siteid=workorder.siteid) or (jobplan.orgid = workorder.orgid and jobplan.siteid is null) or (jobplan.orgid is null and jobplan.siteid is null)) ) as jpriority,  (select top 1 site.description from site, workorder where site.siteid=workorder.siteid) as siteid ,  workorder.targstartdate,workorder.targcompdate,workorder.actfinish, Convert(int, convert(datetime,workorder.actfinish)) - Convert(int, Convert(datetime,workorder.targcompdate)) as delay  from workorder  where 1=1 and ((workorder.siteid like '%YARIMCA%')) and workorder.orgid='YARORG' and (workorder.parent = 'NULL' or workorder.parent is null) and workorder.jpnum is not null and workorder.targstartdate is not null and workorder.targcompdate is not null  and workorder.targcompdate <= { ts '2015-01-01 23:59:59.998' } and workorder.targstartdate >= { ts '2009-01-01 00:00:00.000' }
 
 
 
 
-select * from workorder where reportdate < '01/01/15'
+select * 20 52 ercan ekıcı from workorder where reportdate < '01/01/15'
--- Sorgular/bp_workorder_detail.sql
+++ Sorgular/bp_workorder_detail.sql
@@ -27,3 +27,38 @@ where (woclass = 'WORKORDER' OR woclass = 'ACTIVITY') AND PARENT IS NULL AND his
 
 
 select wonum, description, (select asset.priority from asset where asset.assetnum = workorder.assetnum) from workorder
+
+
+ select wonum,siteid,location,pmnum,  
+ (select person.displayname from person where person.personid = (select top 2 jobplan.supervisor from jobplan where jobplan.jpnum=workorder.jpnum and jobplan.siteid=workorder.siteid)) as owner,
+ (select top 2 asset.priority from asset where asset.assetnum=workorder.assetnum and asset.siteid=workorder.siteid ) as priority,
+ (select top 2 site.description from site where site.siteid=workorder.siteid) as sdescription,
+ (select top 2 pm.description from pm where pm.pmnum=workorder.pmnum and pm.siteid=workorder.siteid) as pdescription,
+ (select top 2 worktype.wtypedesc from worktype where worktype.worktype=workorder.worktype) as worktype,
+ coalesce( (select min(priority) from asset where assetnum in (select assetnum from route_stop where route_stop.route=workorder.route and route_stop.siteid=workorder.siteid) and asset.siteid=workorder.siteid),(select min(priority) from asset where asset.location=workorder.location and asset.siteid=workorder.siteid and workorder.wopriority=5), (select top 2 asset.priority from asset where asset.assetnum=workorder.assetnum and asset.siteid=workorder.siteid)) as minpriority,
+ targstartdate,targcompdate,assetnum,description as description,wopriority,route,jpnum 
+ from workorder
+ where (woclass = 'WORKORDER' or woclass = 'ACTIVITY') and (PARENT IS NULL or parent = 'NULL') AND historyflag = 0 and istask = 0 and status not in ('COMP','CLOSE','CAN') and orgid = 'YARORG'
+
+  select top 2 asset.description,asset.assetnum from asset
+ where asset.assetnum = 'YDT0007' and asset.siteid= 'YARIMCA'
+
+ SELECT * FROM locations WHERE siteid = 'YARIMCA'
+
+ select jobtask.jptask, jobplan.description as jdescription,jobtask.jpnum,jobtask.description as description from jobplan join jobtask on jobplan.jobplanid=jobtask.jobplanid
+ and jobplan.jpnum = 'B0010' and jobplan.siteid= 'YARIMCA'
+
+ select top 2 locations.description,locations.location from locations  
+ where locations.location = 'L10201' and locations.siteid= 'YARIMCA'
+
+ select   (select top 2 priority from asset where asset.assetnum=route_stop.assetnum and asset.siteid=route_stop.siteid) as priority ,
+ (select top 2 description from asset where asset.assetnum=route_stop.assetnum and asset.siteid=route_stop.siteid)as radescription, 
+ route_stop.route as rroute,route_stop.assetnum as rassetnum,
+ coalesce(route_stop.location,(select top 2 asset.location from asset where asset.assetnum=route_stop.assetnum and asset.siteid=route_stop.siteid)) as rlocation, 
+ (select longdescription.ldtext from longdescription where longdescription.ldkey=  (select top 2assetuid from asset where asset.assetnum=route_stop.assetnum and asset.siteid=route_stop.siteid) and ldownertable='ASSET' ) as aldescription,
+ coalesce((select top 2 locations.description from locations where locations.location=(select top 2 asset.location from asset where asset.assetnum=route_stop.assetnum and asset.siteid=route_stop.siteid) and locations.siteid=route_stop.siteid),(select top 2 locations.description from locations where locations.location=route_stop.location and locations.siteid=route_stop.siteid )) as ldescription,
+ (select top 2 routes.description from routes where routes.route=route_stop.route and routes.siteid=route_stop.siteid) as rdescription 
+  from route_stop
+  where route_stop.route='" +rows[0]["route"]+"' and route_stop.siteid= '" +rows[0]["siteid"]+"'  order by priority
+
+
