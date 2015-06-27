e1b7de32c0d65862b80089cb30fccdebd399c5c4
aytunc matrac <aytunc.matrac@hotmail.com.tr>
2015-06-27 17:46:12 +0300

    Turkey delayed wo revision 1
    update

--- Sorgular/Turkey_Delayed_WO.sql
+++ Sorgular/Turkey_Delayed_WO.sql
@@ -1,52 +1,41 @@
-select 
-	workorder.wonum,
-	workorder.description,
-	(select 
-		jobplan.priority 
-	from 
-		jobplan, workorder 
-	where 
-		jobplan.jpnum = workorder.jpnum 
-		and (
-			(jobplan.orgid=workorder.orgid 
-				and jobplan.siteid=workorder.siteid) 
-			or 
-			(jobplan.orgid=workorder.orgid 
-				and jobplan.siteid is null) 
-			or (
-				jobplan.orgid is null 
-					and jobplan.siteid is null)
-			) and jobplan.orgid='YARORG'
-	) as jpriority,
-	(select distinct(site.description) from site, workorder where site.siteid=workorder.siteid and site.orgid='YARORG') as siteid,
-	workorder.targstartdate,
-	workorder.targcompdate,
-	workorder.actfinish,
-	workorder.actfinish-workorder.targcompdate as delay
-from 
-	workorder 
-	where workorder.wonum='TR3021579'
-
+﻿select workorder.wonum, workorder.description,
+(select top 1 jobplan.priority from jobplan, workorder where jobplan.jpnum=workorder.jpnum and ((jobplan.orgid=workorder.orgid and jobplan.siteid=workorder.siteid) or (jobplan.orgid = workorder.orgid and jobplan.siteid is null) or (jobplan.orgid is null and jobplan.siteid is null)) ) as jpriority, 
+(select top 1 site.description from site, workorder where site.siteid=workorder.siteid) as siteid , 
+workorder.targstartdate,workorder.targcompdate,workorder.actfinish,workorder.actfinish-workorder.targcompdate as delay 
+from workorder 
 where --" + params["where"] and 
-	workorder.orgid = 'YARORG' and workorder.parent is null and workorder.jpnum is not null and workorder.targstartdate is not null and workorder.targcompdate is not null
-	and workorder.targcompdate <= '01/01/2009'   --" + MXReportSqlFormat.getEndDayTimestampFunction(params["enddate"]
-	and workorder.targstartdate >= '01/01/2009'  --" + MXReportSqlFormat.getStartDayTimestampFunction(params["startdate"])
+workorder.orgid='YARORG' and (workorder.parent = 'NULL' or workorder.parent is null) and workorder.jpnum is not null and workorder.targstartdate is not null and workorder.targcompdate is not null 
+and workorder.targcompdate <= '01/01/15'--" + MXReportSqlFormat.getEndDayTimestampFunction(params["enddate"])
+and workorder.targstartdate >= '01/01/09'--" + MXReportSqlFormat.getStartDayTimestampFunction(params["startdate"])
 
-select distinct(site.description) from site, workorder where site.siteid=workorder.siteid and workorder.wonum='TR3021579'
 
-select 
-		jobplan.priority 
-	from 
-		jobplan, workorder 
-	where 
-		jobplan.jpnum = workorder.jpnum 
-		and (
-			(jobplan.orgid=workorder.orgid 
-				and jobplan.siteid=workorder.siteid) 
-			or 
-			(jobplan.orgid=workorder.orgid 
-				and jobplan.siteid is null) 
-			or (
-				jobplan.orgid is null 
-					and jobplan.siteid is null)
-			) and jobplan.orgid='YARORG' and workorder.wonum='TR3021579'
+select workorder.wonum, 
+	workorder.description,  
+	(select top 1 jobplan.priority from jobplan, workorder where jobplan.jpnum=workorder.jpnum and ((jobplan.orgid=workorder.orgid and jobplan.siteid=workorder.siteid) or (jobplan.orgid = workorder.orgid and jobplan.siteid is null) or (jobplan.orgid is null and jobplan.siteid is null)) ) as jpriority,   
+	(select top 1 site.description from site, workorder where site.siteid=workorder.siteid) as siteid ,   
+	workorder.targstartdate,workorder.targcompdate,workorder.actfinish,workorder.actfinish-workorder.targcompdate as delay   
+from workorder  
+where 1=1  
+	and workorder.orgid='YARORG' 
+	and (workorder.parent = 'NULL' or workorder.parent is null) 
+	and workorder.jpnum is not null 
+	and workorder.targstartdate is not null 
+	and workorder.targcompdate is not null  
+	and workorder.targcompdate <= '01/01/15' 
+	and workorder.targstartdate >= '01/01/09'
+
+	
+select workorder.wonum, workorder.description,  
+(select top 1 jobplan.priority from jobplan, workorder where jobplan.jpnum=workorder.jpnum and ((jobplan.orgid=workorder.orgid and jobplan.siteid=workorder.siteid) or (jobplan.orgid = workorder.orgid and jobplan.siteid is null) or (jobplan.orgid is null and jobplan.siteid is null)) ) as jpriority,  
+(select top 1 site.description from site, workorder where site.siteid=workorder.siteid) as siteid ,  
+workorder.targstartdate,workorder.targcompdate,workorder.actfinish,workorder.actfinish-workorder.targcompdate as delay   
+from workorder  
+where 1=1 and ((workorder.siteid like '%YARIMCA%')) and workorder.orgid='YARORG' and (workorder.parent = 'NULL' or workorder.parent is null) and workorder.jpnum is not null and workorder.targstartdate is not null and workorder.targcompdate is not null  and workorder.targcompdate <= { ts '2015-01-01 23:59:59.998' } and workorder.targstartdate >= { ts '2009-01-01 00:00:00.000' }
+
+
+0542 308 20 52 ercan ekıcı 
+
+
+
+
+select * from workorder where reportdate < '01/01/15'
